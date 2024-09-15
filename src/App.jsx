afc01f53542774cc326cf8f382612b11f0d48ea2
adri <adri.holzer.e@gmail.com>
2024-09-15 10:56:21 -0300

    commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {EmpleoList} from "./components/EmpleoList"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <div className="card">
+        
+      <EmpleoList/>
+
+
+      </div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// @ts-ignore
+import App from './App';
+import { createRoot } from 'react-dom/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <App />
+)
